19edb6669d6b776bfc0ee9b8a2646433d4eb72e5
Iulian Barbu <iulian.barbu@parity.io>
2024-11-03 00:21:23 +0200

    fix tests
    Signed-off-by: Iulian Barbu <iulian.barbu@parity.io>

--- macros/src/tests.rs
+++ macros/src/tests.rs
@@ -66,7 +66,7 @@ fn test_compile_markdown_valid() {
             .to_string(),
         "\"# This is a markdown file\\n\\n```rust\\nstruct \
         Something;\\n```\\n<!-- this is a comment -->\\n\\n`\
-        ``rust\\nfn some_fn() {\\n    println!(\\\"foo\\\");\
+        ``rust,ignore\\nfn some_fn() {\\n    println!(\\\"foo\\\");\
         \\n}\\n```\\n\\nSome text this is some text\\n\""
     );
 }
@@ -96,7 +96,7 @@ fn test_compile_markdown_source_valid() {
         "this is some markdown\n\
         this is some more markdown\n\
         # this is a title\n\
-        ```rust\n\
+        ```rust,ignore\n\
         fn some_fn() {\n    \
             println!(\"foo\");\n\
         }\n\
